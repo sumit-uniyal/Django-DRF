d90233e0538fb8a3c454ed74debb6c8c7668c61e
Sumit uniyal <suniyal141@gmail.com>
2024-06-26 16:56:28 +0530

    test

--- email_auth/utils.py
+++ email_auth/utils.py
@@ -2,7 +2,7 @@ from django.core.mail import send_mail
 from django.conf import settings
  
 def send_emails(subject, message, email):
-
+ 
     subjects = subject
     messages = message
     from_email = settings.EMAIL_HOST_USER
